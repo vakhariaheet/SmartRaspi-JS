ac61236881f4cc57dd730fed9137f0294898da44
Heet Vakharia <heetkv@gmail.com>
2024-10-25 15:45:36 +0530

    Testing RGB

--- node_modules/@abandonware/bleno/lib/mac/bindings.js
+++
@@ -1,8 +0,0 @@
-const events = require('events');
-const util = require('util');
-
-const BlenoMac = require('./native/binding').BlenoMac;
-
-util.inherits(BlenoMac, events.EventEmitter);
-
-module.exports = new BlenoMac();
